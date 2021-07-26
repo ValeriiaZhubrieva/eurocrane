88c02bda86791d554af39845e3793851c92d4b30
ValeriiaZhubrieva <perchik010102@gmail.com>
2021-07-26 21:31:22 +0300

    added header + adaptive version

--- eurocrane/js/app.js
+++ eurocrane/js/app.js
@@ -68,7 +68,7 @@ if (location.hash) {
 let iconMenu = document.querySelector(".icon-menu");
 if (iconMenu != null) {
 	let delay = 500;
-	let menuBody = document.querySelector(".menu__body");
+	let menuBody = document.querySelector(".menu__list");
 	iconMenu.addEventListener("click", function (e) {
 		if (unlock) {
 			body_lock(delay);
@@ -79,7 +79,7 @@ if (iconMenu != null) {
 };
 function menu_close() {
 	let iconMenu = document.querySelector(".icon-menu");
-	let menuBody = document.querySelector(".menu__body");
+	let menuBody = document.querySelector(".menu__list");
 	iconMenu.classList.remove("_active");
 	menuBody.classList.remove("_active");
 }
